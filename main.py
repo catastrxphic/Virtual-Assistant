721b3ef9c261735aaab8e8ec3c68f17538c263ff
Maria Camila Copo Amador <99522544+catastrxphic@users.noreply.github.com>
2023-05-01 19:11:11 -0400

    added search of people in wikipedia and youtube openning with song

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import speech_recognition as sr
 import pyttsx3, nltk
+import pywhatkit
+import wikipedia
 from nltk.chat.util import Chat, reflections
 
 #initializing speech recognition engine:
@@ -14,48 +16,19 @@ tts_engine.setProperty('voice', voice[1].id)
 voice_rate = tts_engine.getProperty('rate')
 tts_engine.setProperty('rate', 130)
 
-#defining and creating virtual assistant's responses. For this we will use prompt-answer format
-dialogs = [
-    #setting forms of greeting user
-    [
-        r"i am (.*)", ["Hello %1, What can I do for you today?"]
-    ],
-    [
-        r"hey there|hello|how are you|hi", ["Hey, do you need my assistance?"]
-    ],
-    #Creating answers to intro prompt
-    [
-        r"what is your name|how should i call you|do you have a name", ["My name is Nyx, I am your virtual assistant. Nice to meet you!"]
-    ],
-    #Starting to describe actions
-    [
-        r"search for (.*)", ["Searching %1...", "Here is what I found for %1"]
-    ],
-    [
-        r"send a message to (.*) |send an email to (.*)", ["Sure, what should I type?"]
-    ],
-    #Anything else, say that you don't know the answer
-    [
-        r"(.*)", ["I am sorry, I do not understand, do you mind repeating?", "Oops, I did not catch that, repeat please!", "Someone needs to practice diction, try saying it again slower!"]
-    ]
-]
-
-
-#defining chatbot using imported Chat and reflections plus our dialogs (created above)
-chatbot = Chat(dialogs, reflections)
 
 #funtion to hear user's voice input and return text
 def get_audio():
     with sr.Microphone() as source:
-        print("Can I help you with anything? I am listening...")
+        print("Listening...")
         audio = recog_engine.listen(source)
 
         try:
-            message = recog_engine.recognize_google(audio)
-            print("you said: ", message)
-            return message
+            instruction = recog_engine.recognize_google(audio)
+            print("you said: ", instruction)
+            return instruction
         except:
-            print("SOrry, I didn't understand you!")
+            print("Sorry, I didn't understand  !")
             return ""
         
 #funtion for the chatbot to speak the answer to the user
@@ -69,11 +42,23 @@ talk_back("Hello, this is your virtual assistant, how can I help you today?")
 #continue conversation
 while True:
     #get the anser fo the user and process it
-    user_message = get_audio().lower()
+    instructions = get_audio().lower()
 
-    if user_message == "":
-        continue
-    answer = chatbot.respond(user_message)
-    print("Virtual assistant: ", answer)
-    talk_back(answer)
+    #open a video in youtube
+    if "play" in instructions:
+        song = instructions.replace('play', "")
+        talk_back("playing " + song)
+        pywhatkit.playonyt(song)
 
+    elif "hey there" in instructions:
+        talk_back("Hello, how can I help you?")
+    
+    elif "your name" in instructions and 'what' in instructions:
+        print("Virtual assistant: " , "I am nyx, your virtual assistant, Nice to meet you!")
+        talk_back("I am nyx, your virtual assistant, Nice to meet you!")
+
+    elif "tell me about" in instructions:
+        figure = instructions.replace('search for', "")
+        biography = wikipedia.summary(figure, 1)
+        print("virtual assint: ", biography)
+        talk_back(biography)
