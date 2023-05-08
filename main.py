83a4a9e8c6d8d01df280d3e7e1f2b3166b69d4d6
Maria Camila Copo Amador <99522544+catastrxphic@users.noreply.github.com>
2023-05-08 19:39:17 -0400

    google search

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import pywhatkit
 import wikipedia
 import requests
 from bs4 import BeautifulSoup
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 from nltk.chat.util import Chat, reflections
 
 #initializing speech recognition engine:
@@ -41,29 +39,31 @@ def talk_back(message):
     tts_engine.runAndWait()
 
 def search_answer(instructions):
-    #this section function is to extract keywords from user input
-    stop_words = set(stopwords.words('english'))
-    instruction_tokens = word_tokenize(instructions)
-    filtered_words = [word for word in instruction_tokens if not word in stop_words]
-
-    #thjis code section function is to search online
-    query = ' '.join(filtered_words)
-    
-    url_search = f"https://google.com/search?q={query}"
+    #this code section function is to search online
+    query = instructions
+    print("query = ", query)
     headers={
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"
 
-    }
+    } 
+    
+    url_search = f"https://google.com/search?q={query}"
+
+
     answer = requests.get(url_search,headers=headers)
     print("status code= ", answer.status_code)
     
     soup = BeautifulSoup(answer.text, 'html.parser')
-    answers = soup.select('.r a')
-    print("answers = ", answers)
+    answers = []
+    
+    for result in soup.select('.tF2Cxc'):
+        url = result.a['href']
+        title = result.a.text
+        answers.append((title,url))
+
     if len(answers) > 0:
-        top_results = answers[0].get('href')
-        print("top search results: ", + top_results)
-        talk_back("The top search result is: " + top_results)
+        print("top search results: ", + answers)
+        talk_back("The top search result is: " + answers[0][0])
     else:
         talk_back("No search results were found for " + query)
 
@@ -96,5 +96,5 @@ while True:
     elif "tell me about" in instructions:
         figure = instructions.replace('tell me about', "")
         biography = wikipedia.summary(figure, 1)
-        print("virtual assint: ", biography)
+        print("virtual assistant: ", biography)
         talk_back(biography)
